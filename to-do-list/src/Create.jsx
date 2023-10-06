ce813b4f8052007797625b69b4e93afa9c1bc638
maurizio-keningen <mcoronar2100@alumno.ipn.mx>
2023-10-05 19:43:32 -0600

    Commit 2

--- to-do-list/src/Create.jsx
+++ to-do-list/src/Create.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Create() {
   return (
     <div className="create_form">
-      <input type="text" name="" id="" />
+      <input type="text" name="" id="" placeholder='Enter a task' />
       <button type="button">Add</button>
     </div>
   )

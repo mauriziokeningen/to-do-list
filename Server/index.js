f483c259d798aef3a4e5f198c7a4f5a32801620b
maurizio-keningen <mcoronar2100@alumno.ipn.mx>
2023-10-07 00:00:23 -0600

    19.49

---
+++ Server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const TodoModel = require('./Models/Todo')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://coronareyesmauricio20:5vg8mxv02aaNPTjC@cluster0.1bhamwa.mongodb.net/test')
+
+app.post('/add', (req, res) => {
+    const task = req.body.task;
+    TodoModel.create({
+        task: task
+    }).then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+app.listen(1001, ()=> {
+    console.log("Server is Running")
+})
